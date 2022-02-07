92c49f7e1aa5f5bfca9610d2dca89d6a15612199
Joseph Maheshe <maheshejs@gmail.com>
2022-02-07 00:20:25 -0500

    Restructured

--- rtl/tilesrom.v
+++ rtl/tilesrom.v
@@ -84,7 +84,7 @@ module tilesrom (
     altsyncram_component.address_aclr_a = "NONE",
     altsyncram_component.clock_enable_input_a = "BYPASS",
     altsyncram_component.clock_enable_output_a = "BYPASS",
-    altsyncram_component.init_file = "memory_100.mif",
+    altsyncram_component.init_file = "../utils/memory_100.mif",
     altsyncram_component.intended_device_family = "Cyclone V",
     altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
     altsyncram_component.lpm_type = "altsyncram",
